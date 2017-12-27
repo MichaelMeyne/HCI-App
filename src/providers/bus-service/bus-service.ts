ae89e315d0a0bc2a17825334920c8fa18428414e
michael meyne <michael@meyne.co.uk>
2017-12-27 18:43:38 +0000

    Implemented maps for each bus station

--- src/providers/bus-service/bus-service.ts
+++ src/providers/bus-service/bus-service.ts
@@ -16,6 +16,7 @@ export class BusServiceProvider {
 
   private app_id = "91676f07";
   private app_key = "e10dc441385db6f855e3e5ad29bcd6c8";
+  private place_type = "bus_stop";
   private default_lat = 52.6309;
   private default_lon = 1.2974;
   private min_lat = this.default_lat - 0.1;
@@ -23,7 +24,7 @@ export class BusServiceProvider {
   private min_lon = this.default_lon - 0.1;
   private max_lon = this.default_lon + 0.1;
 
-  private url : string = `https://transportapi.com/v3/uk/places.json?app_id=${this.app_id}&app_key=${this.app_key}&max_lat=${this.max_lat}&max_lon=${this.max_lon}&min_lat=${this.min_lat}&min_lon=${this.min_lon}&type=bus_stop`;
+  private url : string = `https://transportapi.com/v3/uk/places.json?app_id=${this.app_id}&app_key=${this.app_key}&max_lat=${this.max_lat}&max_lon=${this.max_lon}&min_lat=${this.min_lat}&min_lon=${this.min_lon}&type=${this.place_type}`;
 
   constructor(private http: Http) {
     console.log('Hello BusServiceProvider Provider');
--- src/providers/train-service/train-service.ts
+++ src/providers/train-service/train-service.ts
@@ -14,7 +14,17 @@ import {Observable} from 'rxjs/Observable'
 @Injectable()
 export class TrainServiceProvider {
 
-  private url : string = "http://transportapi.com/v3/uk/places.json?query=&type=train_station&app_id=91676f07&app_key=e10dc441385db6f855e3e5ad29bcd6c8";
+  private app_id = "91676f07";
+  private app_key = "e10dc441385db6f855e3e5ad29bcd6c8";
+  private place_type = "train_station";
+  private default_lat = 52.6309;
+  private default_lon = 1.2974;
+  private min_lat = this.default_lat - 0.1;
+  private max_lat = this.default_lat + 0.1;
+  private min_lon = this.default_lon - 0.1;
+  private max_lon = this.default_lon + 0.1;
+
+  private url : string = `https://transportapi.com/v3/uk/places.json?app_id=${this.app_id}&app_key=${this.app_key}&max_lat=${this.max_lat}&max_lon=${this.max_lon}&min_lat=${this.min_lat}&min_lon=${this.min_lon}&type=${this.place_type}`;
 
   constructor(private http: Http) {
     console.log('Hello TrainServiceProvider Provider');
